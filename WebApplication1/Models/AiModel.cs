abca1fc004b0269401cc3378acdd3a413526e61c
TheSadFreedom <nikita.kun.1.6@gmail.com>
2024-06-12 23:44:54 +0300

    Залил

---
+++ WebApplication1/Models/AiModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebApplication1.Models;
+
+public partial class AiModel
+{
+    public long AiModelId { get; set; }
+
+    public long? AiModelAuthor { get; set; }
+
+    public string? AiModelName { get; set; }
+
+    public string? AiModelDescription { get; set; }
+}
